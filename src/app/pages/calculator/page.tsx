707a40a8a82a1b9818af101e049459c629f1c24d
JoniMeichtry11 <joni.meichtry@gmail.com>
2024-07-07 16:12:47 -0300

    feat: add calculator page

--- src/app/pages/calculator/page.tsx
+++ src/app/pages/calculator/page.tsx
@@ -1,12 +1,23 @@
+import Button from '@/app/components/Button';
 import React from 'react';
 
 const Calculator = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold">About Us</h1>
-      <p className="mt-4 text-lg">
-        This is the about page.
-      </p>
+    <div className="container p-4 h-screen">
+      <div className='flex flex-col justify-center gap-8 items-center'>
+        <h1 className='text-5xl font-bold text-center mb-8'>Calculadora</h1>
+        <Button href="/pages/calculator/docena">
+          Calcular por docena
+        </Button>
+        <Button href="/pages/calculator/media-docena">
+          Calcular por media docena
+        </Button>
+        <Button href="/pages/calculator/unidad">
+          Calcular por unidad
+        </Button>
+        <h2 className='font-bold text-red-500 text-4xl'>¡¡¡IMPORTANTE!!!</h2>
+        <h3 className='text-2xl'>Recordar al cliente que la oferta <strong>SIEMPRE</strong> es comprando la docena de churros.</h3>
+      </div>
     </div>
   );
 };
