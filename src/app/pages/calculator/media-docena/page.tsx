c588ba3289ec21c693f0bb7c945b878cf3f5be16
JoniMeichtry11 <joni.meichtry@gmail.com>
2024-07-07 16:13:38 -0300

    feat: add calculator page by the dozen, half dozen and unity

---
+++ src/app/pages/calculator/media-docena/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React, { useState } from 'react';
+import CustomNumberInput from '@/app/components/CustomNumberInput';
+
+const Docena = () => {
+  const [simplesValue, setSimplesValue] = useState<number>(0);
+  const [dulceDeLecheValue, setDulceDeLecheValue] = useState<number>(0);
+
+  const precioSimples = 3000;
+  const precioDulceDeLeche = 3500;
+
+  const totalPrice = ((simplesValue * precioSimples) + (dulceDeLecheValue * precioDulceDeLeche)).toFixed(2);
+  
+  return (
+    <div className="container mx-auto p-4 h-screen">
+      <div className="flex flex-col justify-center gap-8 items-center bg-gray-100 rounded-lg shadow-md p-8">
+        <h1 className="text-3xl md:text-5xl font-bold text-center mb-8 text-brown">Calculadora por Media Docena</h1>
+        <div className='flex flex-col items-center'>
+          <h3 className="text-lg md:text-2xl text-center text-brown">Churros Simples</h3>
+          <CustomNumberInput value={simplesValue} onValueChange={setSimplesValue} />
+        </div>
+        <div className='flex flex-col items-center'>
+          <h3 className="text-lg md:text-2xl text-center text-brown">Churros Dulce de Leche</h3>
+          <CustomNumberInput value={dulceDeLecheValue} onValueChange={setDulceDeLecheValue} />
+        </div>
+        <div className="text-lg md:text-xl text-center mt-4">
+          <h4>Precio Total: <strong>${totalPrice}</strong></h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Docena;
